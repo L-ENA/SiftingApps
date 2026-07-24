ce83bb72e55f736a2a3b85f69898921d42794d6b
L-ENA <lena.schmidt.0493@gmail.com>
2026-07-24 13:38:16 +0100

    Comment out delete_today function
    Comment out the delete_today function and its call.

--- utils.py
+++ utils.py
@@ -36,30 +36,30 @@ import pandas as pd
 
 import datetime
 import numpy as np
-def delete_today():
-    df_full=pd.read_csv(r"H:\Downloads\2025-06-03T10-59_export.csv")
-    print(df_full.shape)
+# def delete_today():
+#     df_full=pd.read_csv(r"H:\Downloads\2025-06-03T10-59_export.csv")
+#     print(df_full.shape)
 
-    today = datetime.date.today()
-
-
-    # df['date_column'] = pd.to_datetime(df['date'], utc=True)
-    # timestamp_value = pd.Timestamp(today, tz='UTC')
-    df_full['date'] = pd.to_datetime(df_full['date'])
-    df = df_full[df_full['date'].dt.date != today]
-
-    cutoff_date = pd.Timestamp.now() - pd.Timedelta(days=8)
-    #df[date_column] = pd.to_datetime(df[date_column])
-    df=df[df['date'].values.astype(np.datetime64) >= cutoff_date]
-
-    # # Now the comparison should work
-    # df[df['date_column'] < timestamp_value]
+#     today = datetime.date.today()
 
 
-    print(df.shape)
-    df.to_csv(r"H:\Downloads\2025-06-03_export_{}.csv".format(df.shape[0]), index=False, encoding='utf-8-sig')
-    # mydiff=df_full.drop(df.index, axis=0)
-    # print(mydiff.shape)
-    # mydiff.to_csv(r"H:\Downloads\mydiff.csv")
+#     # df['date_column'] = pd.to_datetime(df['date'], utc=True)
+#     # timestamp_value = pd.Timestamp(today, tz='UTC')
+#     df_full['date'] = pd.to_datetime(df_full['date'])
+#     df = df_full[df_full['date'].dt.date != today]
 
-delete_today()
+#     cutoff_date = pd.Timestamp.now() - pd.Timedelta(days=8)
+#     #df[date_column] = pd.to_datetime(df[date_column])
+#     df=df[df['date'].values.astype(np.datetime64) >= cutoff_date]
+
+#     # # Now the comparison should work
+#     # df[df['date_column'] < timestamp_value]
+
+
+#     print(df.shape)
+#     df.to_csv(r"H:\Downloads\2025-06-03_export_{}.csv".format(df.shape[0]), index=False, encoding='utf-8-sig')
+#     # mydiff=df_full.drop(df.index, axis=0)
+#     # print(mydiff.shape)
+#     # mydiff.to_csv(r"H:\Downloads\mydiff.csv")
+
+# delete_today()
